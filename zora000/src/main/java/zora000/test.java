cc8d0e3cbde5bef7ee12a7da908dddde63646687
duannazora <50536084+duannazora@users.noreply.github.com>
2019-05-12 12:34:58 +0800

    Update test.java

--- zora000/src/main/java/zora000/test.java
+++ zora000/src/main/java/zora000/test.java
@@ -3,6 +3,7 @@ package zora000;
 public class test {
 	public static void main(String[] args) {
 	System.out.println("4454");
-	System.out.println(333);
+	System.out.println(333333333333333333333333333333333);
+
 	}
 }
